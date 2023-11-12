8a20969cd3e6d29c8f6fbd8d124ec0b7e39ed00c
Crosbic <thecrosbic@gmail.com>
2023-11-12 12:28:45 +0300

    fix: модалка открывается при нажатии на всю кнопку, а не только на текст

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -14,52 +14,52 @@ const Header = () => {
   const handleClose = () => setOpen(false)
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.header_container}>
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            alt="logo"
-            width="213"
-            height="17"
-            className={styles.header_conteiner__logo}
-          />
-        </Link>
-        <div className={styles.header_container__buttons_group}>
+    <>
+      <ModalLogin open={open} onClose={handleClose} />
+
+      <div className={styles.wrapper}>
+        <div className={styles.header_container}>
           <Link href="/">
-            <Typography>Обмен</Typography>
+            <Image
+              src="/logo.png"
+              alt="logo"
+              width="213"
+              height="17"
+              className={styles.header_conteiner__logo}
+            />
           </Link>
-          <Link href="/">
-            <Typography>Партнёрам</Typography>
-          </Link>
-          <Link href="/">
-            <Typography>Резервы</Typography>
-          </Link>
-          <Link href="/">
-            <Typography>Новости</Typography>
-          </Link>
-          <Link href="/">
-            <Typography>Контакты</Typography>
-          </Link>
-        </div>
-        <div className={styles.header_container__buttons_group__auth_group}>
-          <Link href="/">
+          <div className={styles.header_container__buttons_group}>
+            <Link href="/">
+              <Typography>Обмен</Typography>
+            </Link>
+            <Link href="/">
+              <Typography>Партнёрам</Typography>
+            </Link>
+            <Link href="/">
+              <Typography>Резервы</Typography>
+            </Link>
+            <Link href="/">
+              <Typography>Новости</Typography>
+            </Link>
+            <Link href="/">
+              <Typography>Контакты</Typography>
+            </Link>
+          </div>
+          <div className={styles.header_container__buttons_group__auth_group}>
             <div
               className={
                 styles.header_container__buttons_group__auth_group__button_signin__bg
               }
+              onClick={handleOpen}
             >
               <Typography
                 className={
                   styles.header_container__buttons_group__auth_group__button_signin__text
                 }
-                onClick={handleOpen}
               >
                 Вход
               </Typography>
             </div>
-          </Link>
-          <Link href="/">
             <div
               className={
                 styles.header_container__buttons_group__auth_group__button_reg_bg
@@ -73,24 +73,25 @@ const Header = () => {
                 Регистрация
               </Typography>
             </div>
-          </Link>
-        </div>
-        <div className={styles.header_container__langauge_choice_container}>
-          <Image
-            src="/ru-icon-circle.png"
-            alt="ru-icon"
-            width="16"
-            height="16"
-          />
-          <Typography
-            className={styles.header_container__langauge_choice_container__text}
-          >
-            Русский &#9660;
-          </Typography>
+          </div>
+          <div className={styles.header_container__langauge_choice_container}>
+            <Image
+              src="/ru-icon-circle.png"
+              alt="ru-icon"
+              width="16"
+              height="16"
+            />
+            <Typography
+              className={
+                styles.header_container__langauge_choice_container__text
+              }
+            >
+              Русский &#9660;
+            </Typography>
+          </div>
         </div>
       </div>
-      <ModalLogin open={open} onClose={handleClose} />
-    </div>
+    </>
   )
 }
 
